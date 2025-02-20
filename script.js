655c0cf6d1bf50bb48b6f75c59454c427508a936
joshpee22 <owobujoshua2017@gmail.com>
2025-02-20 05:02:40 +0100

    Initia commit

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 let navLinks = document.getElementById("navLinks");
-let menuIcon = document.querySelector(".fa");
 
-function toggleMenu() {
-    // Check the current state of the menu (whether it's hidden or visible)
-    if (navLinks.style.top === "0px") {
-        navLinks.style.top = "-800px"; // Hide the menu
-    } else {
-        navLinks.style.top = "0"; // Show the menu
-    }
+function showMenu() {
+  navLinks.style.top = '0'; // Show menu
 }
 
-// Event listener for clicking the menu icon
-menuIcon.addEventListener("click", toggleMenu);
+function hideMenu() {
+  navLinks.style.top = '-800px'; // Hide menu
+}
